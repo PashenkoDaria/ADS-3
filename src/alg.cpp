f32da20e5f0585feaac84672cd481f3104936bf4
PashenkoDaria <92744987+PashenkoDaria@users.noreply.github.com>
2022-05-15 16:20:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int left = 0, right = size - 1, centr, count = 0, k = -1;
+while (left <= right) {
+  centr = left + (right - left) / 2;
+  if (value == arr[centr]) {
+    count++;
+    k = centr;
+    break;
+  }
+  if (value > arr[centr]) {
+    left = centr + 1;}
+  else
+    right = centr - 1;
+  }
+  if (k >= 0) {
+  for (int i = 0; i < k; i++) {
+    if (value == arr[i])
+    count++;
+  }
+  for (int j = size - 1; j > k; j--) {
+    if (value == arr[j])
+    count++;
+  }
+  }
+  if (count > 0)
+    return count;
+  else
+    return 0; // если ничего не найдено
 }
